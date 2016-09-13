091c0d42d78035f0f34e9f35a221cf7c0b6f887d
Chris Dawes <cmsd2@cantab.net>
2016-09-13 13:39:17 +0100

    add script to generate x509 signing cert

---
+++ src/OpenIdConnectServer/Scripts/gen-certs.ps1
@@ -0,0 +1,3 @@
+openssl genrsa "-out" private.pem 2048
+openssl req "-x509" "-new" "-key" private.pem "-out" public.pem
+openssl pkcs12 "-export" "-in" public.pem "-inkey" private.pem "-out" cert.pfx
